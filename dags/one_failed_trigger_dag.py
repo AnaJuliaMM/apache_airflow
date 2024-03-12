adfa542cbbf3ee1e5f1e63907bf23386435829d2
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-03-12 14:27:24 -0300

    feat: pipeline with one failed trigger rule

---
+++ dags/one_failed_trigger_dag.py
@@ -0,0 +1,29 @@
+# Imports 
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime
+
+# DAG object
+with  DAG('trigger_rule_dag', 
+          description='This DAG handle with trigger rules',
+          schedule_interval=None,
+          start_date=datetime(2024, 3, 12),
+          catchup=False) as dag:
+
+    # Tasks
+    task1 = BashOperator(
+        task_id='task_1',
+        bash_command='sleep 5')
+
+    task2 = BashOperator(
+        task_id='task_2',
+        bash_command='exit 1')
+
+    task3 = BashOperator(
+        task_id='task_3',
+        bash_command='sleep 5',
+        trigger_rule='one_failed'
+        )
+
+    # Flow
+    [task1, task2] >> task3
