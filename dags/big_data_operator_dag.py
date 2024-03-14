80b85ac9c15f062959123f5324976c4e6743835a
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-03-14 16:53:52 -0300

    feat: implement dag to use big data operator

---
+++ dags/big_data_operator_dag.py
@@ -0,0 +1,21 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime
+from big_data_operator import BigDataOperator
+
+# DAG object
+dag = DAG('bigdata_operator_dag', 
+          description='My first plugin operator ',
+          schedule_interval=None,
+          start_date=datetime(2024, 3, 12),
+          catchup=False)
+
+# Tasks
+big_data = BigDataOperator(
+    task_id='big_data',
+    file_path = '/opt/airflow/data/Churn.csv',
+    destination_path = '/opt/airflow/data/Churn.parquet',
+    dag=dag
+)
+
+big_data
