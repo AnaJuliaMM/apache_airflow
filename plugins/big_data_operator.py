43ead1c601b302956f277a8d0fea4a22184c6ec2
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-03-14 16:45:56 -0300

    chore(plugin): create big data operator

---
+++ plugins/big_data_operator.py
@@ -0,0 +1,22 @@
+from airflow.models import BaseOperator 
+from airflow.utils.decorators import apply_default
+import pandas as pd 
+
+class BigDataOperator(BaseOperator):
+    
+    @apply_default
+    def __init__(self,  file_path, destination_path, separator=';', file_type='parquet', *args, **kwargs):
+        super().__init__( *args, **kwargs)
+        self.file_path = file_path
+        self.destination_path = destination_path
+        self.separator = separator 
+        self.file_type = file_type
+        
+    def execute(self, context):
+        dataframe = pd.read_csv(self.file_path, sep=self.separator)
+        if self.file_type == 'parquet':
+            dataframe.to_parquet(self.destination_path)
+        elif self.file_type == 'json':
+            dataframe.to_json(self.destination_path)
+        else:
+            raise ValueError('Tipo inválidp')
